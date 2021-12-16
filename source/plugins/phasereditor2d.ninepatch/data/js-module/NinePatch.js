dcb6daa52d4cc761a8b65b1686f088d7ca8aa9b4
Arian Fornaris <boniatillo@gmail.com>
2021-12-16 14:17:31 -0500

    Patch version.

--- source/plugins/phasereditor2d.ninepatch/data/js-module/NinePatch.js
+++ source/plugins/phasereditor2d.ninepatch/data/js-module/NinePatch.js
@@ -1,4 +1,4 @@
-// version: 1.0.0
+// version: 1.0.1
 import Phaser from "phaser";
 export default class NinePatch extends Phaser.GameObjects.RenderTexture {
     textureKey;
--- source/plugins/phasereditor2d.ninepatch/data/js-module/registerNinePatchFactory.js
+++ source/plugins/phasereditor2d.ninepatch/data/js-module/registerNinePatchFactory.js
@@ -1,4 +1,4 @@
-// version: 1.0.0
+// version: 1.0.1
 import Phaser from "phaser";
 export default function registerNinePatchFactory() {
     Phaser.GameObjects.GameObjectFactory.register("ninePatch", function (x, y, width, height, key, frame) {
